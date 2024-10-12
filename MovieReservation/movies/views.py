b26ee48540278238c2a6592bf157ec9b63e73e30
Hossein <hosseinabedinipoco@gmail.com>
2024-10-12 18:41:41 +0330

    add movie view

--- MovieReservation/movies/views.py
+++ MovieReservation/movies/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .serializers import MovieSerializer
+from rest_framework import status
 # Create your views here.
 
 class add_movie(APIView):
     permission_classes = [IsAuthenticated, IsAdminUser]
     def post(self, request):
-        pass
+        movie = MovieSerializer(data=request.data)
+        if movie.is_valid():
+            movie.save()
+            return Response(movie.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(movie.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class update_movie(APIView):
     permission_classes = [IsAuthenticated, IsAdminUser]
