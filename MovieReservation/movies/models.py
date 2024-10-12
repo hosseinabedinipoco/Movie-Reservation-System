4a4c817f5ad2875769ca1890a4d746c6bc3db108
Hossein <hosseinabedinipoco@gmail.com>
2024-10-12 18:20:00 +0330

    create movie model

--- MovieReservation/movies/models.py
+++ MovieReservation/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    director = models.CharField(max_length=50)
+    time_in_minute = models.PositiveIntegerField()
+    
+
+    def __str__(self):
+        return self.title
