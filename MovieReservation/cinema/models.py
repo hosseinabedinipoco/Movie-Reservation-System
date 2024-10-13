409f50358e1c991722bfe385622d6bd587c58d38
Hossein <hosseinabedinipoco@gmail.com>
2024-10-13 17:44:37 +0330

    add cinema model

--- MovieReservation/cinema/models.py
+++ MovieReservation/cinema/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Create your models here.
+class Cinema(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    capacity = models.IntegerField()
+    Address = models.CharField(max_length=100)
+    rate = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.name
+    
--- MovieReservation/movies/models.py
+++ MovieReservation/movies/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=50)
@@ -7,6 +7,8 @@ class Movie(models.Model):
     director = models.CharField(max_length=50)
     time_in_minute = models.PositiveIntegerField()
     genre = models.CharField(max_length=10, default="")
+    rate = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(5)])
+
 
     def __str__(self):
         return self.title
