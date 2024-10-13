6c4c42fb12a0490d0d54477e271f0e8003245418
Hossein <hosseinabedinipoco@gmail.com>
2024-10-13 17:48:22 +0330

    set urls for cinema

--- MovieReservation/cinema/urls.py
+++ MovieReservation/cinema/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    
+    path('add', views.Add_cinema.as_view()),
+    path('update/<int:id>', views.Update_Cinema.as_view()),
+    path('delete/<int:id>', views.delete_Cinema.as_view()),
 ]
--- MovieReservation/cinema/views.py
+++ MovieReservation/cinema/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import APIView
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import Is_admin
 # Create your views here.
+
+class Add_cinema(APIView):
+    permission_classes = [Is_admin, IsAuthenticated]
+
+class Update_Cinema(APIView):
+    permission_classes = [IsAuthenticated, Is_admin]
+
+class delete_Cinema(APIView):
+    permission_classes = [Is_admin, IsAuthenticated]        
