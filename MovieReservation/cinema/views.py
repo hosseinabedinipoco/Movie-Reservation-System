cd681b82dde032039628804ba6ed51ca0c0e14e1
Hossein <hosseinabedinipoco@gmail.com>
2024-10-13 18:00:13 +0330

    add cinema view

--- MovieReservation/cinema/views.py
+++ MovieReservation/cinema/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from rest_framework.decorators import APIView
 from rest_framework.permissions import IsAuthenticated
 from users.permissions import Is_admin
+from rest_framework.response import Response
+from .serializers import CinemaSerializer
+from rest_framework import status
 # Create your views here.
 
 class Add_cinema(APIView):
     permission_classes = [Is_admin, IsAuthenticated]
+    def post(self, request):
+        cinema = CinemaSerializer(data=request.data)
+        if cinema.is_valid():
+            cinema.save()
+            return Response(cinema.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(cinema.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class Update_Cinema(APIView):
     permission_classes = [IsAuthenticated, Is_admin]
+    
 
 class delete_Cinema(APIView):
     permission_classes = [Is_admin, IsAuthenticated]        
