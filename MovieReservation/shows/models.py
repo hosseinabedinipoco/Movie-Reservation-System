c2faef6c7ec735f9255613dc4ea6defe4c40aba1
Hossein <hosseinabedinipoco@gmail.com>
2024-10-16 17:29:38 +0330

    create show model

--- MovieReservation/shows/models.py
+++ MovieReservation/shows/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from movies.models import Movie
+from cinema.models import Cinema
 # Create your models here.
+class Show(models.Model):
+    time = models.DateTimeField()
+    cinema = models.ForeignKey(Cinema, on_delete=models.DO_NOTHING)
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.movie + " in " + self.cinema + " at " + self.time
+
