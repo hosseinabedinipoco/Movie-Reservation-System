a6d90ac61694bce0a26618cd719c4e86d8c948b6
Hossein <hosseinabedinipoco@gmail.com>
2024-10-16 17:14:45 +0330

    delete seat model

--- MovieReservation/cinema/admin.py
+++ MovieReservation/cinema/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Cinema, Seat
+from .models import Cinema
 # Register your models here.
 admin.site.register(Cinema)
-admin.site.register(Seat)
--- MovieReservation/cinema/urls.py
+++ MovieReservation/cinema/urls.py
@@ -3,6 +3,5 @@ from . import views
 urlpatterns = [
     path('add', views.Add_cinema.as_view()),
     path('update/<int:id>', views.Update_Cinema.as_view()),
-    path('delete/<int:id>', views.delete_Cinema.as_view()),4
-    path
+    path('delete/<int:id>', views.delete_Cinema.as_view()),
 ]
