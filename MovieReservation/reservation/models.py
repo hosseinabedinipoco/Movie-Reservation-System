4b7fb0df6446a8074c268ba65e5a5a4edbdab0dd
Hossein <hosseinabedinipoco@gmail.com>
2024-10-16 17:33:30 +0330

    create reservation model

--- MovieReservation/reservation/models.py
+++ MovieReservation/reservation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from users.models import User
+from shows.models import Show
 # Create your models here.
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    show = models.ForeignKey(Show, on_delete=models.DO_NOTHING)
+    seat_number = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.user + " in " + self.show + " on seat " + self.seat_number
